b9150bc24a80587a576e812aa531bb793ffcf69e
mrityunjay mishra <mrityunjay.83039@gmail.com>
2023-12-29 16:18:38 +0530

    Add and remove in watch list button functionality done

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,9 +1,25 @@
 import React, { useEffect, useState } from "react";
-import johnwickPoster from "../johnwick-poster.jpg";
 import axios from "axios";
 
 function Movies() {
   const [movies, setMovies] = useState([]);
+  const [watchList, setWatchList] = useState([]);
+
+  //handle watchlist methods
+
+  let addToWatchList = (id) => {
+    const newWatchList = [...watchList, id];
+    setWatchList(newWatchList);
+  };
+
+  let removeFromWatchList = (id) => {
+    let filteredWatchListId = watchList.filter((watchListId) => {
+      return watchListId != id;
+    });
+    setWatchList(filteredWatchListId);
+  };
+
+  console.log(watchList);
 
   // Fetch API Data
   useEffect(() => {
@@ -16,15 +32,6 @@ function Movies() {
     });
   }, []);
 
-
-  // add to watchlist 
-
-  let addToWatchList = (e)=>{
-    
-    e.target.innerHTML = '<span style="color:red;text-align: center;position: relative;top: -3px;">x</span>'
-  }
-
-
   return (
     <section>
       <h1 className="text-center font-bold text-2xl m-8 underline decoration-double">
@@ -33,14 +40,32 @@ function Movies() {
       <div className="flex flex-wrap">
         {movies.map((movie) => {
           return (
-            <div key={movie.id}
+            <div
+              key={movie.id}
               className="w-[200px] h-[35vh] bg-center bg-cover m-3 rounded-xl md:h[40vh] md:w[200px] hover:scale-110 duration-300 relative flex items-end"
               style={{
                 backgroundImage: `url(https://image.tmdb.org/t/p/original/t/p/w500/${movie.poster_path})`,
               }}>
+              <p className="text-white font-bold text-center w-full bg-gray-900 bg-opacity-60">
+                {movie.title}
+              </p>
+              {/* Show hide add to watch list and remove from watch list button  */}
 
-              <p className="text-white font-bold text-center w-full bg-gray-900 bg-opacity-60">{movie.title}</p>
-              <div onClick={addToWatchList} className="text-2xl bg-gray-900 rounded-2xl absolute right-2 top-2 h-8 w-8 text-center">😍</div>
+              {watchList.includes(movie.id) ? (
+                <div
+                  onClick={() => removeFromWatchList(movie.id)}
+                  className="text-2xl bg-gray-900 rounded-2xl absolute right-2 top-2 h-8 w-8 text-center text-red-600">
+                  ✖
+                </div>
+              ) : (
+                <div
+                  onClick={() => addToWatchList(movie.id)}
+                  className="text-2xl bg-gray-900 rounded-2xl absolute right-2 top-2 h-8 w-8 text-center">
+                  😍
+                </div>
+              )}
+
+              {/* Show hide add to watch list and remove from watch list button  */}
             </div>
           );
         })}
