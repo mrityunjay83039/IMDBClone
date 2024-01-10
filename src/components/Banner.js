925ab14b10c8cf0ead6b20e9dc29530b3971e39b
mrityunjay mishra <mrityunjay.83039@gmail.com>
2024-01-10 13:20:14 +0530

    Banner Image Change

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
+import bannerImg from "../banner.jpeg";
 
-let Banner = ()=>{
-    return(
-        <div className='relatice'>
-            <img className='w-full h-96' src='https://assets-in.bmscdn.com/discovery-catalog/events/et00311714-ewdhvajezf-landscape.jpg'/>
-            <p className='text-white absolute bottom-40 w-full text-center text-2xl font-bold bg-black bg-opacity-20 p-4'>John Wick</p>
-        </div>
-    )
-}
+let Banner = () => {
+  return (
+    <div className="relatice">
+      <img className="w-full h-[65vh]" src={bannerImg} />
+      
+    </div>
+  );
+};
 
 export default Banner;
